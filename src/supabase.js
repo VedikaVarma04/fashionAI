b494d36095014fee1f488c6d5c46fb7743ed65f4
VedikaVarma04 <vedikavarma93@gmail.com>
2026-01-19 20:31:32 +0530

    Fixed base paths for deployment

--- src/supabase.js
+++ src/supabase.js
@@ -1,14 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.ITE_SUPABASE_URL
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(
-  "https://hvsxhbkrkabewwljjjqi.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2c3hoYmtya2FiZXd3bGpqanFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MTA2MDgsImV4cCI6MjA4MDE4NjYwOH0.HuIjFkPN4tI9eg6DVzZV0kxyqG9gVQNXm-kpQpWtfr8"
-);
-
-
+  "https://yngtlxsohhvziiwhaaaq.supabase.co",
+  "sb_publishable_vnt-EAQ3Jq4uUaVcfRZT0w_mHoFwlXK"
+)
 
 
 
